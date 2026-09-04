b531fe760ba928f5a5c5aef919ecf3f72c5e94c1
Moises-Salgado <moises.salgado15@gmail.com>
2026-09-04 09:06:48 -0400

    Cambio mínimo

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel, Field
 from app.schemas.users import UserRead
 
-
 class LoginRequest(BaseModel):
     username: str = Field(min_length=1)
     password: str = Field(min_length=1)
